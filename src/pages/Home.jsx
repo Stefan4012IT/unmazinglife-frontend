ad7d877969a234bc7d068e00925fc63636bac4cf
stefan4012it <stefanra555@gmail.com>
2025-02-09 12:32:37 +0100

    Section-1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,10 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Header from '../components/Header/Header'
 import BtnPrime from '../components/Buttons/BtnPrime';
+import img_1 from '../assets/content_img_1of4.jpg';
+import img_2 from '../assets/content_img_2of4.jpg';
+import img_3 from '../assets/content_img_3of4.jpg';
+import img_4 from '../assets/content_img_4of4.jpg';
 
 const Home = () => {
 
@@ -27,6 +31,40 @@ const Home = () => {
                 <BtnPrime text="Join Today"/>
                 </div>
             </div>
+        </div>
+        <div className='section-1'>
+          <div className="container">
+            <div className="main-box wrapper">
+              <div className="text-box">
+                <div className='text-box--content' data-aos="fade-down">
+                  <div className="heading">
+                    <span className='subtitle'>About App</span>
+                    <h2>Segment stranice sa generalnim opisom <span className='mask'>Unmaze</span> aplikacije</h2>
+                  </div>
+                  <p>Pellentesque mi sed amet ac arcu venenatis dignissim magnis. Est eget massa id urna arcu ac. Eget porttitor feugiat mauris tempus eget velit congue. Et eu diam fringilla a vitae. Pellentesque mi sed amet ac arcu venenatis dignissim magnis. Est eget massa id urna arcu ac. Eget porttitor feugiat mauris tempus eget velit congue. Et eu diam fringilla a vitae.Pellentesque mi sed amet ac arcu venenatis dignissim magnis.</p>
+                </div>
+                <BtnPrime text="Learn More"/>
+              </div>
+              
+              
+              <div className='img-box-container' data-aos="zoom-in">
+                <div className="img-group-1">
+                  <img className='logoHeader img_1' src={img_1} alt='img_1' />
+                </div>
+
+                <div className="img-group-2">
+                  <img className='logoHeader img_3' src={img_3} alt='img_3' />
+                  <img className='logoHeader img_2' src={img_2} alt='img_2' />
+
+                  <img className='logoHeader img_4' src={img_4} alt='img_4' />
+                </div>
+
+              </div>
+            </div>
+            
+          </div>
+
+
         </div>
     </>
     
