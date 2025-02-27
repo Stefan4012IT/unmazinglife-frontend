b7b49d705e1d409dd9a1fbc845814bf0d44d286f
stefan4012it <stefanra555@gmail.com>
2025-02-27 19:46:14 +0100

    blogPost

---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+const BlogPage = () => {
+
+
+    useEffect(() => {
+        AOS.init({
+            // ovde možete dodati globalna podešavanja za AOS
+            easing: 'linear',
+            duration: 1500, // trajanje animacije
+        });
+        }, []);
+  return (
+    <>
+       <Header />
+        {/* HERO-SECTION --- pocetak */}
+        <div className="blog-header-section">
+            <div className="container">
+                <div className="main-box wrapper">
+                <div className='main-box--content' data-aos="fade-right">
+                    <h1>Main slogan goes here lorem ipsum</h1>
+                    <p>Pellentesque mi sed amet ac arcu venenatis dignissim magnis. Est eget massa id urna arcu ac. Eget porttitor feugiat mauris tempus eget velit congue. Et eu diam fringilla a vitae.</p>
+                </div>
+                </div>
+            </div>
+        </div>
+        {/* HERO-SECTION --- kraj */}
+
+
+       <Footer />  
+    </>
+  )
+}
+
+export default BlogPage
