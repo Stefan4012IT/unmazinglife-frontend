603d4bae67a3bf81bfe987a874799f011fe0c033
stefan4012it <stefanra555@gmail.com>
2025-03-03 15:40:28 +0100

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/main.scss'; 
 import Home from './pages/Home';
 import BookLanding from './pages/BookLanding'; 
